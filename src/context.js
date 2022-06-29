0ee518ac59bafd4ed2e625a411216b880d037011
Shresth Rahul <shresth7001@gmail.com>
2022-06-29 19:08:56 +0530

    Add files via upload

---
+++ src/context.js
@@ -0,0 +1,45 @@
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSideBar = () => {
+    setIsSideBarOpen(true);
+  };
+
+  const closeSideBar = () => {
+    setIsSideBarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSideBarOpen,
+        openModal,
+        openSideBar,
+        closeModal,
+        closeSideBar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+//custom hooks
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
